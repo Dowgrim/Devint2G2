0b01eff33728f8afb653ba5e32492886def96f71
Lydwen <tomlau94@aol.com>
2015-03-25 13:12:10 +0100

    1er jet de Player.java, création du fichier de test : testPlayer.java

---
+++ Proto/test/modele/testPlayer.java
@@ -0,0 +1,15 @@
+package modele;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testPlayer {
+
+    @Test
+    public void test() {
+        
+       assertTrue(true);
+    }
+
+}
